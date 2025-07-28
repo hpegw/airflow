528d693695e5d5cd0d775648f22a6df25a8c7813
Gerhard Wenzel <64268332+hpegw@users.noreply.github.com>
2025-07-28 09:30:13 +0200

    Update check_dsv_ingest_connection.py

--- check_dsv_ingest_connection.py
+++ check_dsv_ingest_connection.py
@@ -5,4 +5,4 @@ import logging
 conn = BaseHook.get_connection("dsv_ingest")
 path = conn.extra_dejson.get("path")
 
-logging.info(f""Path from connection:: {path}")
+logging.info(f"Path from connection:: {path}")
