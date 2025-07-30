b7c034614084bb709d022a63bc9cf6cefd71ad07
Gerhard Wenzel <64268332+hpegw@users.noreply.github.com>
2025-07-30 06:19:54 +0200

    Update DSV_OCR.py

--- DSV_OCR.py
+++ DSV_OCR.py
@@ -469,8 +469,8 @@ with DAG(
     )
 
     convert_tables_to_json = PythonOperator(
-        task_id="convert_merged_tabled_to_json",
-        python_callable=convert_tables_to_json
+        task_id="convert_tables_to_json",
+        python_callable=convert_merged_tables_to_json
     )
 
 
